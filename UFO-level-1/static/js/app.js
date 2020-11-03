d0fee68f1cd65b6e437850358c62fa9628306998
Zuzana Svorenova <zuzana.svorenova@gmail.com>
2020-11-03 12:11:18 -0600

    filter added output to console

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -26,3 +26,23 @@ tableData.forEach((i) => {
     });
 });
 
+// Complete the event handler function for the form
+function runFilter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+  
+    console.log(inputValue);
+    // console.log(tableData);
+  
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+  
+    console.log(filteredData);
+
+  };
