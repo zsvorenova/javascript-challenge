b6cb76966a30b72a4ca7d92771f0d1f378f6717c
Zuzana Svorenova <zuzana.svorenova@gmail.com>
2020-11-03 11:58:24 -0600

    read appending table data to html

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,19 @@
 // from data.js
 var tableData = data;
 
+var tbody = d3.select("tbody");
+
 // YOUR CODE HERE!
+tableData.forEach((i) => {
+    // console.log(i);
+    // append row for each ufo sighting case
+    var row = tbody.append("tr");
+
+    Object.entries(i).forEach(([key, value]) => {
+        // console.log (key, value);
+        // append a cell to the row for each value in the ufo sightings dataset
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
