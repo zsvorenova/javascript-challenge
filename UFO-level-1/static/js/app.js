70d73ac660bcef2aaa178a152fa13ea6c3075a3b
Zuzana Svorenova <zuzana.svorenova@gmail.com>
2020-11-03 23:04:02 -0600

    small corrections

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -8,7 +8,7 @@ var button = d3.select("#filter-btn");
 // Select the form
 var form = d3.select("form");
 // Select tabel area
-var tableArea=d3.select("#table-area")
+var tableArea = d3.select("#table-area")
 
 // Create event handlers 
 button.on("click", runFilter);
@@ -36,33 +36,32 @@ function runFilter() {
 
     // Prevent the page from refreshing
     d3.event.preventDefault();
-    
+
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
-  
+
     // Get the value property of the input element
     var inputValue = inputElement.property("value");
-  
+
     console.log(inputValue);
 
     // to ensure that full tabel will load after clearing the form we add the if condition
     if (inputValue === "") {
         var filteredData = tableData;
-    }
-    else {
+    } else {
         var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
         console.log(filteredData);
-    };    
+    };
 
     // remove data(any children) from the table body
     tbody.html("");
+    tableArea.selectAll('p').html("");
 
     // append new data for selected day or text if no data is found
     if (filteredData.length !== 0) {
         console.log("writing table");
         writeTable(filteredData);
-    }
-    else {
+    } else {
         console.log("no data exists");
         tableArea.append("p").text("No such data exist!");
     };
